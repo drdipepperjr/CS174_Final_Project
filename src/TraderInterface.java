107b441717d3470bd390a687450845f4d8614d2f
Rebecca Lee <rebeccalee@Rebeccas-MacBook-Pro-2.local>
2017-12-01 05:08:18 -0800

    external moviedb and reviewdb functions working

--- src/TraderInterface.java
+++ src/TraderInterface.java
@@ -5,10 +5,14 @@ import java.text.DecimalFormat;
 public class TraderInterface{
     
     public static final String HOST = "jdbc:mysql://cs174a.engr.ucsb.edu:3306/domenicdipeppeDB";
+    public static final String MOVIE = "jdbc:mysql://cs174a.engr.ucsb.edu:3306/moviesDB";
+    public static final String REVIEW= "jdbc:mysql://cs174a.engr.ucsb.edu:3306/reviewsDB";
     public static final String USER = "domenicdipeppe";
     public static final String PWD = "907";
     
     Connection connection = null;
+    Connection movieconnection = null;
+    Connection reviewconnection = null;
     private String username =null;
     private String password =null;
     
@@ -35,12 +39,12 @@ public class TraderInterface{
             
             connection = DriverManager.getConnection(HOST, USER, PWD);
             
-            PreparedStatement customer = connection.prepareStatement("SELECT * from Customers WHERE username=username");
+            PreparedStatement customer = connection.prepareStatement("SELECT * FROM Customers WHERE username=username");
             ResultSet rs = customer.executeQuery();
             while (rs.next()){
                 taxID=rs.getInt("taxid");
             }
-            PreparedStatement account = connection.prepareStatement("SELECT * from Accounts WHERE taxid= taxID");
+            PreparedStatement account = connection.prepareStatement("SELECT * FROM Accounts WHERE taxid= taxID");
             rs = account.executeQuery();
             while (rs.next()){
                 accountid=rs.getInt("accountid");
@@ -136,24 +140,89 @@ public class TraderInterface{
                         break;
                         
                     }
-                    
+                    */
                     //List movie information
                     else if(choice.equals("List Movie Information")||choice.equals("List movie information")||choice.equals("list movie information")){
+                        System.out.println("What movie do you need info on?: ");
+                        String movietitle = scan.nextLine();
+                        movieconnection = DriverManager.getConnection(MOVIE, USER, PWD);
+                        PreparedStatement mc = movieconnection.prepareStatement("SELECT * FROM Movies WHERE title=?");
+                        mc.setString(1,movietitle);
+                        ResultSet mcrs = mc.executeQuery();
+                        if(!mcrs.first()){
+                            System.out.println("No movies with this name in moviesDB");
+                        }
+                        else{
+                            mcrs.first();
+                            float rating=mcrs.getFloat("rating");
+                            System.out.println("Rating: " + rating);
+                            int year= mcrs.getInt("production_year");
+                            System.out.println("Year: " + year);
+                        }
                         break;
                         
                     }
                     
                     //Get Top movies
                     else if(choice.equals("Get Top Movies")||choice.equals("Get top movies")||choice.equals("get top movies")){
+                        System.out.println("Starting from what year: ");
+                        int startYear = scan.nextInt();
+                        System.out.println("To what year: ");
+                        int endYear = scan.nextInt();
+                        
+                        movieconnection = DriverManager.getConnection(MOVIE, USER, PWD);
+                        while (endYear-startYear>=0){
+                            PreparedStatement mc = movieconnection.prepareStatement("SELECT * FROM Movies WHERE production_year=? AND rating=5");
+                            mc.setInt(1,startYear);
+                            ResultSet mcrs = mc.executeQuery();
+
+                            while(mcrs.next()){
+                                String title=mcrs.getString("title");
+                                System.out.print("Title: " + title);
+                                int year= mcrs.getInt("production_year");
+                                System.out.println("   Year: " + year);
+                            }
+                            startYear++;
+
+                        }
+                        System.out.println("End of list for top moves");
                         break;
                         
                     }
                     
                     //Get Reviews
                     else if(choice.equals("Get Reviews")||choice.equals("Get reviews")||choice.equals("get reviews")){
+                        System.out.println("What movie do you want reviews for?: ");
+                        String movietitle = scan.nextLine();
+                        movieconnection = DriverManager.getConnection(MOVIE, USER, PWD);
+                        PreparedStatement mc = movieconnection.prepareStatement("SELECT * FROM Movies WHERE title=?");
+                        mc.setString(1,movietitle);
+                        ResultSet mcrs = mc.executeQuery();
+                        int id=0;
+                        if(!mcrs.first()){
+                            System.out.println("No movies with this name in moviesDB");
+                        }
+                        else{
+                            id= mcrs.getInt("id");
+                        }
+
+                        reviewconnection = DriverManager.getConnection(MOVIE, USER, PWD);
+                        PreparedStatement rc = reviewconnection.prepareStatement("SELECT * FROM Reviews WHERE movie_id=?");
+                        rc.setInt(1,id);
+                        ResultSet rcrs = rc.executeQuery();
+                        while (rcrs.next()){
+                            String reviewAuthor=rcrs.getString("author");
+                            String reviewText=rcrs.getString("review");
+                            System.out.println("Author: " + reviewAuthor +"  Review: " + reviewText);
+                            
+                        }
+                        
+                        System.out.println("End of reviews.");
+
+
                         break;
                     }
-                    */
+                    
                     
                     //logout
                     else if(choice.equals("Logout")||choice.equals("logout")){
@@ -161,7 +230,7 @@ public class TraderInterface{
                         break;
                     }
                     else {
-                        System.out.print("Not An Option");
+                        System.out.println("Not An Option");
                         break;
                     }
                 }
